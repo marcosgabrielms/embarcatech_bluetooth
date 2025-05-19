0a04267a1157538dba0e4a624b1fb1b482e0f093
Marcos <marcosgabriel166@gmail.com>
2025-05-19 20:40:40 -0300

    :sparkles: feat:  adiciona nova lógica de programação

--- src/bluetooth/bluetooth.c
+++ src/bluetooth/bluetooth.c
@@ -1,11 +1,5 @@
 #include "bluetooth.h"
-#include "hardware/uart.h"
-#include "hardware/gpio.h"
 
-#define UART_ID uart1
-#define BAUD_RATE 9600
-#define UART_TX_PIN 18  // GP4 → RX do HC-05
-#define UART_RX_PIN 19  // GP8 ← TX do HC-05
 
 void bluetooth_init(void) {
     uart_init(UART_ID, BAUD_RATE);
@@ -20,4 +14,22 @@ bool bluetooth_available(void) {
 
 char bluetooth_read_char(void) {
     return uart_getc(UART_ID);
+}
+
+void bluetooth_read_string(uart_inst_t *uart, char *buffer, size_t max_len) {
+    size_t i = 0;
+
+    while (i < max_len - 1) {
+        // Espera até receber um caractere
+        char c = uart_getc(uart);
+
+        // Se for Enter (\r ou \n), encerra a string
+        if (c == '\r' || c == '\n') {
+            break;
+        }
+
+        buffer[i++] = c;
+    }
+
+    buffer[i] = '\0'; // Termina a string
 }
--- src/bluetooth/bluetooth.h
+++ src/bluetooth/bluetooth.h
@@ -1,7 +1,15 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
+#include "hardware/uart.h"
+#include "hardware/gpio.h"
 #include <stdbool.h>
+
+#define UART_ID uart1
+#define BAUD_RATE 9600
+#define UART_TX_PIN 18  // GP4 → RX do HC-05
+#define UART_RX_PIN 19  // GP8 ← TX do HC-05
+
 
 // Inicializa a UART para comunicação com o módulo HC-05
 void bluetooth_init(void);
@@ -11,5 +19,7 @@ bool bluetooth_available(void);
 
 // Lê um caractere da UART Bluetooth
 char bluetooth_read_char(void);
+
+void bluetooth_read_string(uart_inst_t *uart, char *buffer, size_t max_len);
 
 #endif // BLUETOOTH_H
